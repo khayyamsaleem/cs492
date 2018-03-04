6cd8916de954cfdf319a7be67b05599472c65d0a
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-03-04 10:30:59 -0500

    fixed links

--- p1/include/prodcons.h
+++ p1/include/prodcons.h
@@ -1,1 +1,43 @@
-../../cs492-workspace/prodcons/include/prodcons.h
+#ifndef PRODCONS_INCLUDE
+#define PRODCONS_INCLUDE
+
+#include <time.h>
+#include <sstream>
+
+typedef struct Product {
+	unsigned producer_id;
+    unsigned product_id;
+    float timestamp;
+    unsigned life;
+    explicit Product(const unsigned _producer_id, const unsigned _product_id, const clock_t _timestamp, const unsigned _life){
+    	    producer_id = _producer_id;
+        product_id = _product_id;
+        timestamp = ((float)_timestamp)/CLOCKS_PER_SEC;
+        life = _life%1024;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Product &prod){
+        os << "Product "
+           << prod.product_id
+		   << " produced by Producer "
+		   << prod.producer_id
+           << " has timestamp "
+           << prod.timestamp
+           << " and life "
+           << prod.life << "." << std::endl;
+        os.flush();
+        return os;
+    }
+} Product;
+
+extern int num_producers ;
+extern int num_consumers ;
+extern int num_products  ;
+extern int queue_size    ;
+extern int s_algo        ;
+extern int quantum       ;
+extern int seed          ;
+
+void *producer(void *);
+//void *consumer(void *);
+
+#endif
