df3a18354eb2d6785026d97bffa657149a5fd2a6
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-03-04 10:24:35 -0500

    implemented threads, checkpoint

--- p1/include/prodcons.h
+++ p1/include/prodcons.h
@@ -2,16 +2,38 @@
 #define PRODCONS_INCLUDE
 
 #include <time.h>
+#include <sstream>
 
 typedef struct Product {
-    int product_id;
+    unsigned product_id;
     float timestamp;
     unsigned life;
-    Product(int _product_id, clock_t _timestamp, unsigned _life){
+    explicit Product(const unsigned _product_id, const clock_t _timestamp, const unsigned _life){
         product_id = _product_id;
         timestamp = ((float)_timestamp)/CLOCKS_PER_SEC;
         life = _life%1024;
     }
+    friend std::ostream& operator<<(std::ostream& os, const Product &prod){
+        os << "Product "
+           << prod.product_id
+           << " has timestamp "
+           << prod.timestamp
+           << " and life "
+           << prod.life << ". ";
+        os.flush();
+        return os;
+    }
 } Product;
+
+extern int num_producers ;
+extern int num_consumers ;
+extern int num_products  ;
+extern int queue_size    ;
+extern int s_algo        ;
+extern int quantum       ;
+extern int seed          ;
+
+void *producer(void *);
+void *consumer(void *);
 
 #endif
